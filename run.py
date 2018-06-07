4761414b68193cb8c9e1a91425daffda15e9d417
L1V1NGH311 <williamblakelock.wb@gmail.com>
2018-06-07 18:55:05 +1200

    Update run.py

--- run.py
+++ run.py
@@ -246,13 +246,16 @@ async def try_ban(user,reason,message):
         await client.ban(banee,0)
 
 async def try_kick(user,reason,channel):
+    try:
     kickee = channel.server.get_member(user)
-    if kickee.permissions_in(channel).administrator:
-        await client.send_message(channel,"I can't do that, they are an administrator!")
-        return
-    else:
-        await client.send_message(kickee,"You were kicked for the following reason: {}".format(reason))
-        await client.kick(kickee)
+        if kickee.permissions_in(channel).administrator:
+            await client.send_message(channel,"I can't do that, they are an administrator!")
+            return
+        else:
+            await client.send_message(kickee,"You were kicked for the following reason: {}".format(reason))
+            await client.kick(kickee)
+    except Exception as e:
+        await client.send_message(channel,'An error has occured: {}'.format(error))
 
 async def try_warn(user,reason,message):
     warnee = message.channel.server.get_member(user)
