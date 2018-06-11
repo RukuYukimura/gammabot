544a195d365d21971adf90ba722806e10f73ae15
L1V1NGH311 <williamblakelock.wb@gmail.com>
2018-06-11 23:06:00 +1200

    Update run.py

--- run.py
+++ run.py
@@ -10,8 +10,12 @@ async def on_message(message):
         return
     if message.content.startswith('hey 8 ball'):
         await client.send_message(message.channel,await fun.magic8ball())
-    if message.content.startswith('roll a d'):
+    elif message.content.startswith('roll a d'):
         await client.send_message(message.channel,"You rolled a... {}".format(str(await fun.roll(int(message.content.replace('roll a d',''))))))
+    elif message.content.startswith('!choose'):
+        choices = message.content.replace('!choose','')
+        choices = choices.strip(',')
+        await client.send_message(message.channel,"I chose: {}".format(await fun.choice(choices)))
     pass
 
 client.run(os.environ['TOKEN'])
