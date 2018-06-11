3acc0f54b77326b4811deccc1d2f5f997bbcb579
L1V1NGH311 <williamblakelock.wb@gmail.com>
2018-06-11 22:14:37 +1200

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ async def on_message(message):
     if message.author.bot:
         return
     if message.content.startswith('hey 8 ball'):
-        await client.send_message(message.channel,fun.magi8ball())
+        await client.send_message(message.channel,fun.magic8ball())
     pass
 
 client.run(os.environ['TOKEN'])
