3db5cd59fab6168deb57d4cd7188396b1e6f30b6
L1V1NGH311 <williamblakelock.wb@gmail.com>
2018-06-11 22:10:12 +1200

    Update fun.py

--- gamma/fun.py
+++ gamma/fun.py
@@ -1,1 +1,15 @@
+import discord
+from random import randint
+from datetime import date
 
+async def magic8ball():
+  reactions = {
+    1: 'no',
+    2: 'yes',
+    3: 'try again'
+  }
+  return reactions[randint(1,len(reactions)-1)]
+
+if __name__ == "__main__":
+  import sys
+  sys.exit(-1)
