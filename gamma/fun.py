f921c6a9a2ea96b43f7c65345ba987f96d395b11
L1V1NGH311 <williamblakelock.wb@gmail.com>
2018-06-11 22:25:42 +1200

    Update fun.py

--- gamma/fun.py
+++ gamma/fun.py
@@ -4,9 +4,23 @@ from datetime import date
 
 async def magic8ball():
   reactions = {
-    1: 'no',
-    2: 'yes',
-    3: 'try again'
+    1: 'It is certain.',
+    2: 'Most likely.',
+    3: 'Without a doubt.',
+    4: 'Signs point to yes.',
+    5: 'You may rely on it.',
+    6: 'Reply hazy try again.',
+    7: 'Ask again later.',
+    8:'Better not tell you now.',
+    9: 'Cannot predict now.',
+    10: 'Concentrate and ask again.',
+    11: 'Don\'t count on it.',
+    12: 'My reply is no.',
+    13: 'My sources say no.',
+    14: 'Outlook not so good.',
+    15: 'Very doubtful.',
+    16: 'No u.',
+    17: 'No me.'
   }
   return reactions[randint(1,len(reactions)-1)]
 
