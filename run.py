c270b72611f1993cc215bde54a1404dfb9b67660
L1V1NGH311 <williamblakelock.wb@gmail.com>
2018-06-11 22:12:55 +1200

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,10 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith('hey 8 ball'):
+        await client.send_message(message.channel,fun.magi8ball())
     pass
 
 client.run(os.environ['TOKEN'])
